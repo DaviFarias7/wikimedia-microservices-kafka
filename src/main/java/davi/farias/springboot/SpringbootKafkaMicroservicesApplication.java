e23f54f0e21ce821e25d32433c378590df0450b7
davi.farias <davi.farias@unimedjp.com.br>
2024-03-28 17:18:07 -0300

    first commit

---
+++ src/main/java/davi/farias/springboot/SpringbootKafkaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package davi.farias.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davi/farias/springboot/SpringbootKafkaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package davi.farias.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
